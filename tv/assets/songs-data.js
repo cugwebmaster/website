86d75d71bb5ff6167e61a3830ffe5d7fb71c4afb
cugwebmaster <cugwebmaster@gmail.com>
2026-05-12 16:34:27 +1200

    Add files via upload

---
+++ tv/assets/songs-data.js
@@ -0,0 +1,299 @@
+const songsData = [
+  {
+    "id": "song-1",
+    "number": "1.",
+    "title": "Advertising Jingles",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/1-Advertising-Jingles.pdf",
+    "dataName": "1. Advertising Jingles",
+    "chords": ["D7"],
+    "speed": 17,
+    "zoom": 2.20,
+    "scenes": [
+    {
+      "at": 0.64,     
+      "goto": 0.01,    
+      "for": 0,
+      "then": "continue"
+    }
+  ]
+  },
+  {
+    "id": "song-2",
+    "number": "2.",
+    "title": "Aunty Alice",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/2-Aunty-Alice.pdf",
+    "dataName": "2. Aunty Alice",
+    "chords": ["D7"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-3",
+    "number": "3.",
+    "title": "April Sun In Cuba",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/3-April-Sun-In-Cuba.pdf",
+    "dataName": "3. April Sun In Cuba",
+    "chords": ["asus4","bm","gm7"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-4",
+    "number": "4.",
+    "title": "Cheryl Moana Marie",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/4-Cheryl-Moana-Marie.pdf",
+    "dataName": "4. Cheryl Moana Marie",
+    "chords": ["D7"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-5",
+    "number": "5.",
+    "title": "Damn The Dam",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/5-Damn-The-Dam.pdf",
+    "dataName": "5. Damn The Dam",
+    "chords": ["em","bm","e","b7"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-6",
+    "number": "6.",
+    "title": "Dance All Around The World",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/6-Dance-All-Around-The-World.pdf",
+    "dataName": "6. Dance All Around The World"
+  },
+  {
+    "id": "song-7",
+    "number": "7.",
+    "title": "Do The Blue Beat",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/7-Do-The-Blue-Beat.pdf",
+    "dataName": "7. Do The Blue Beat"
+  },
+  {
+    "id": "song-8",
+    "number": "8.",
+    "title": "Dont You Know Yockomo",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/8-Dont-You-Know-Yockomo.pdf",
+    "dataName": "8. Dont You Know Yockomo"
+  },
+  {
+    "id": "song-9",
+    "number": "9.",
+    "title": "Down The Hall on",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/9-Down-The-Hall-on.pdf",
+    "dataName": "9. Down The Hall on"
+  },
+  {
+    "id": "song-10",
+    "number": "10.",
+    "title": "Forever Tuesday Morning",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/10-Forever-Tuesday-Morning.pdf",
+    "dataName": "10. Forever Tuesday Morning",
+    "chords": ["cm","em"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-11",
+    "number": "11.",
+    "title": "Hands Of My Heart",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/11-Hands-Of-My-Heart.pdf",
+    "dataName": "11. Hands Of My Heart",
+    "chords": ["D7"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-12",
+    "number": "12.",
+    "title": "I See Red",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/12-I-See-Red.pdf",
+    "dataName": "12. I See Red"
+  },
+  {
+    "id": "song-13",
+    "number": "13.",
+    "title": "Island Queen",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/13-Island-Queen.pdf",
+    "dataName": "13. Island Queen"
+  },
+  {
+    "id": "song-14",
+    "number": "14.",
+    "title": "Love in a Fowlhouse",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/14-Love-in-a-Fowlhouse.pdf",
+    "dataName": "14. Love in a Fowlhouse"
+  },
+  {
+    "id": "song-15",
+    "number": "15.",
+    "title": "Poi E",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/15-Poi-E.pdf",
+    "dataName": "15. Poi E"
+  },
+  {
+    "id": "song-16",
+    "number": "16.",
+    "title": "Pokarekare Ana",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/16-Pokarekare-Ana.pdf",
+    "dataName": "16. Pokarekare Ana"
+  },
+  {
+    "id": "song-17",
+    "number": "17.",
+    "title": "Put Another Log on",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/17-Put-Another-Log-on.pdf",
+    "dataName": "17. Put Another Log on"
+  },
+  {
+    "id": "song-18",
+    "number": "18.",
+    "title": "Pretty Girl",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/18-Pretty-Girl-.pdf",
+    "dataName": "18. Pretty Girl",
+     "chords": ["e7"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-19",
+    "number": "19.",
+    "title": "Shes a Mod",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/19-Shes-a-Mod.pdf",
+    "dataName": "19. Shes a Mod",
+    "chords": ["dm"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-20",
+    "number": "20.",
+    "title": "Six Months in a Leaky Boat",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/20-Six-Months-in-a-Leaky-Boat.pdf",
+    "dataName": "20. Six Months in a Leaky Boat"
+  },
+  {
+    "id": "song-21",
+    "number": "21.",
+    "title": "Slice of Heaven",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/21-Slice-of-Heaven.pdf",
+    "dataName": "21. Slice of Heaven",
+    "chords": ["em"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-22",
+    "number": "22.",
+    "title": "Tea at Te Kuiti",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/22-Tea-at-Te-Kuiti.pdf",
+    "dataName": "22. Tea at Te Kuiti",
+    "chords": ["D7"],
+    "speed": 17,
+    "zoom": 2.20,
+  },
+  {
+    "id": "song-23",
+    "number": "23.",
+    "title": "The Gumboot Song",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/23-The-Gumboot-Song.pdf",
+    "dataName": "23. The Gumboot Song",
+    "chords": ["Dm"],
+    "speed": 17,
+    "zoom": 2.20
+  },
+  {
+    "id": "song-24",
+    "number": "24.",
+    "title": "Tumblin Down",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/24-Tumblin-Down.pdf",
+    "dataName": "24. Tumblin Down"
+  },
+  {
+    "id": "song-25",
+    "number": "25.",
+    "title": "Tutira Mai Nga Iwi",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/25-Tutira-Mai-Nga-Iwi.pdf",
+    "dataName": "25. Tutira Mai Nga Iwi",
+    "speed": 17,
+    "zoom": 2.20,
+    "scenes": [
+    {
+      "at": 0.8,     
+      "goto": 0.01,    
+      "for": 0,
+      "then": "continue"
+    }
+  ]
+  },
+  {
+    "id": "song-26",
+    "number": "26.",
+    "title": "Hine e Hine",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/26-Hine-e-Hine.pdf",
+    "dataName": "26. Hine e Hine"
+  },
+  {
+    "id": "song-27",
+    "number": "27.",
+    "title": "National Anthem",
+    "artist": "NA",
+    "genre": "NA",
+    "pdf": "pdfs/27-National-Anthem.pdf",
+    "dataName": "27. National Anthem"
+  }
+];
